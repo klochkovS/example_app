e70ca7f834be3352f0150c5fe2c5f0e7648a879c
klochkovS <fluurr@gmail.com>
2018-07-14 01:11:58 +0300

    Added drag n drop without intersection

--- src/Components/Canvas/Canvas.jsx
+++ src/Components/Canvas/Canvas.jsx
@@ -8,7 +8,7 @@ class Canvas extends Component {
     super(props);
     this.state = {
       currentId: '',
-    }
+    };
     this.handleDoubleClick = this.handleDoubleClick.bind(this);
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
@@ -35,10 +35,43 @@ class Canvas extends Component {
 
   handleMouseMove(event) {
     const { currentId } = this.state;
-    const { changeCoord } = this.props;
+    const { rectangles, changeCoord } = this.props;
     let { clientX, clientY } = event;
     clientX -= 50;
     clientY -= 25;
+    rectangles.forEach((val) => {
+      const { id, x, y } = val;
+      if (id !== currentId) {
+        const left = clientX > x - 100 && clientX < x;
+        const right = clientX < x + 100 && clientX > x;
+        const top = clientY > y - 50 && clientY < y;
+        const bottom = clientY < y + 50 && clientY > y;
+        const conLeft = x - 100;
+        const conTop = y - 50;
+        const conBottom = y + 50;
+        const conRight = x + 100;
+
+        if (left && (top || bottom)) {
+          if (clientY > y - 25 && clientY < y + 25) {
+            clientX = conLeft;
+          } else if (top) {
+            clientY = conTop;
+          } else {
+            clientY = conBottom;
+          }
+        }
+
+        if (right && (top || bottom)) {
+          if (clientY > y - 25 && clientY < y + 25) {
+            clientX = conRight;
+          } else if (top) {
+            clientY = conTop;
+          } else {
+            clientY = conBottom;
+          }
+        }
+      }
+    });
     changeCoord(currentId, clientX, clientY);
   }
 
